fbae15bc83ab764574fa3fc9d791b8e73710eeed
Tony <tony.albert789@gmail.com>
2023-08-26 10:07:03 -0300

    feat: add folders struture

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { server } from "./server/Server";
 
 
-server.listen(3000, () => {
-    console.log("Listening on port 3000");
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
 });
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,12 +1,11 @@
-import express from "express";
+import express from "express"; 
+import "dotenv/config";
+
+import { router } from "./routes";
 
 const server = express();
 
-server.get("/", (req, res) => {
-    res.json({ message: "Hello World" });
-})
-
-
-
+server.use(express.json());
+server.use(router);
 
 export { server };
